47b82a073eaac4f5ebef5c16c1d118c3d0f10fd6
Ontri <gautham.ontri@accenture.com>
2019-05-24 13:44:45 +1000

    dark theme

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, Flask, jsonify, json
 )
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -13,8 +15,15 @@ def hello_world():
 @app.route('/API')
 def apifunc():
 	api = "THIS API is HALO"
-	return render_template("person.html", api=api )
+	url = "https://el0yb5zcg9.execute-api.ap-southeast-2.amazonaws.com/default"
+	payload={"data": "137,10232,55,1,2,1,0,1,44,0,5000,3,1,250,40,9.3333333,0.8"}
+
+	r = requests.post(url, data=json.dumps(payload))
+	result = r.json()
+	result = result["predictions"][0]["score"]
+
+	return render_template("person.html", api=result )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+	app.run(host="0.0.0.0", port=80)
 
