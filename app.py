67db8cd41a70336b1d296bcaf34e9b0a4e882e93
reuben.lema <reuben.lema@accenture.com>
2019-05-24 14:40:45 +1000

    Merged

--- app.py
+++ app.py
@@ -7,6 +7,53 @@ import json
 
 app = Flask(__name__)
 
+ID = {
+    10124: "77,10124,72,1,2,0,0,0,45,0,40000,1,1,6,21,9.333333333,0",
+    19148: "137,19148,72,1,2,1,0,1,31,0,20000,4,1,250,21,10,0.6,1",
+    19864: "87,19864,4,4,1,0,0,0,31,0,0,4,1,250,6,4.666666667,0"
+
+}
+
+Personal = {
+    10124: {
+        "firstname": "Gamora",
+        "lastname": "Mohegan",
+        "email": "GaryJMohegan@gmail.com",
+        "age": 49,
+        "race": "Native American",
+        "gender": "female",
+        "income": "$100 000+",
+        "education": "Bachelors Degree",
+        "city": "St Louis",
+        "postcode": "63108"
+    },
+    19148: {
+        "firstname": "Caroline",
+        "lastname": "Ardist",
+        "email": "CarolineJArdist@dayrep.com",
+        "age": 31,
+        "race": "Caucasian or White",
+        "gender": "male",
+        "income": "$20 000- $30 000",
+        "education": "Associate Degree",
+        "city": "North Kansa City",
+        "postcode": 64116
+    },
+    19864: {
+        "firstname": "Augustina",
+        "lastname": "Boles",
+        "email": "AugustinaMBoles@einrot.com",
+        "age": 31,
+        "race": "Latino or Hispanic",
+        "gender": "male",
+        "income": "Unspecified",
+        "education": "Associates Degree",
+        "city": "St. Petersburg",
+        "postcode": 33716
+    }
+}
+
+
 @app.route('/')
 def hello_world():
 	return render_template("home.html")
@@ -22,8 +69,30 @@ def apifunc():
 	result = r.json()
 	result = result["predictions"][0]["score"]
 
+
+
+
 	return render_template("person.html", api=result )
 
+@app.route('/search', methods=('GET', 'POST') )
+def searchfun():
+	if request.method == 'POST':
+		name = request.form[ 'user_id' ]
+
+		url = "https://el0yb5zcg9.execute-api.ap-southeast-2.amazonaws.com/default"
+		#payload={"data": "137,10232,55,1,2,1,0,1,44,0,5000,3,1,250,40,9.3333333,0.8"}
+
+		r = requests.post(url, data=json.dumps(payload))
+		result = r.json()
+		result = result["predictions"][0]["score"]
+
+
+
+		return render_template("person.html", api=name )
+
+
+
+
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=80)
+	app.run(host="0.0.0.0", port=81)
 
