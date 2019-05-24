56f6a7804a6652f0bdf85514675218d132bbcdd7
Ontri <gautham.ontri@accenture.com>
2019-05-24 14:26:29 +1000

    dark theme

--- app.py
+++ app.py
@@ -28,6 +28,15 @@ def apifunc():
 def searchfun():
 	if request.method == 'POST':
 		name = request.form[ 'user_id' ]
+
+		url = "https://el0yb5zcg9.execute-api.ap-southeast-2.amazonaws.com/default"
+		#payload={"data": "137,10232,55,1,2,1,0,1,44,0,5000,3,1,250,40,9.3333333,0.8"}
+
+		r = requests.post(url, data=json.dumps(payload))
+		result = r.json()
+		result = result["predictions"][0]["score"]
+
+
 		return render_template("person.html", api=name )
 
 if __name__ == "__main__":
